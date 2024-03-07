8a247488e89b12a22b9894e1f0037964544e820b
Rohithmittu <103620418+Rohithmittu@users.noreply.github.com>
2024-03-07 22:19:17 +0530

    started a project TODO with redux to understand it

---
+++ reduxtoolkit/src/features/todo/todoSlice.js
@@ -0,0 +1,11 @@
+import {createSlice,nanoid} from '@reduxjs/toolkit'
+
+const initialState = {
+    todos: [],
+}
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers:  
+})
